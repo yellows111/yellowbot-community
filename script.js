fcb0389937e27ebefc9d3cf9129974795ea180d2
Declan <dbrady377@gmail.com>
2017-12-26 21:38:57 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function OnEvent_Initialize(MessengerStart)
+{
+}
+
+function OnEvent_Uninitialize(MessengerExit)
+{
+}
+
+// Add Stuff Below.
+// Best to read the Messenger Plus! Scripting Documentation before you edit.
